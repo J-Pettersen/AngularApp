44956777fc0392191ebfadf1986700886e240655
JPettersen <JayPettersen@live.co.uk>
2020-07-14 17:02:50 +0100

    added page-not-found component to display when routing invalid nav is entered

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UserListComponent },
-  { path: 'users/:id', component: UserDetailsComponent }
+  { path: 'users/:id', component: UserDetailsComponent },  
+  { path: '**',   component: PageNotFoundComponent }
 ];
 
 @NgModule({
@@ -17,4 +19,5 @@ const routes: Routes = [
 export class AppRoutingModule { }
 //list of routing components.
 export const routingComponents = [UserListComponent,
-                                  UserDetailsComponent]
+                                  UserDetailsComponent,
+                                  PageNotFoundComponent]
